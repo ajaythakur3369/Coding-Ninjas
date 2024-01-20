52d1e14126add734f316860e84b6f2d68be6492d
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 21:45:40 +0530

    Add files via upload

---
+++ 3_January_2024.py
@@ -0,0 +1,30 @@
+'''
+1. Which of the following is not a Constraint in SQL? 
+Answer:- Union (Constraints are – Primary Key, Not Null, Check)
+'''
+
+'''
+2. What will the output of the following statement be? 
+'''
+
+5 > 4 and 4 > 3 and 3 < 2 
+
+'''
+Output:- False
+'''
+
+'''
+3. Which algorithm is defined in Time quantum? 
+Answer:- round-robin scheduling algorithm 
+'''
+
+'''
+4. Which of the following operators is the right associative? 
+Answer:- assignment operator 
+'''
+
+'''
+5. In the following question, four words have been given of which three are alike in some way and one is different. Choose the odd one out
+– 396, 462, 572, 427, 671, 264 
+Answer:- 427
+'''

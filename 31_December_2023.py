2bbfc1c4092a5551ae0a56071e5378fe9badb78d
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 21:03:27 +0530

    Add files via upload

---
+++ 31_December_2023.py
@@ -0,0 +1,24 @@
+'''
+2. What will be the output of the following code? 
+'''
+
+i = 0
+while i < 5:
+    i = i + 1
+    print(i, end=" ")
+    i += 1
+
+'''
+Output:- 1 3 5 
+'''
+
+'''
+3. Method overriding can occur through only? 
+Answer:- Inheritance 
+'''
+
+'''
+4. During a prize distribution ceremony, Vikram was ninth from the left while Janhvi was eighth from the right in the front row. If Hariom was thirteenth from the left and was exactly in the middle of Vikram and Janhvi in the same row then what was the total number of people in the front row?
+Answer:- 24
+'''
+

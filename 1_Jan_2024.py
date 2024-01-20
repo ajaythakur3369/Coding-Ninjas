078eb35c9e13c0dd8829d983c9f60ba1eb9062c6
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 21:13:16 +0530

    Add files via upload

---
+++ 1_Jan_2024.py
@@ -0,0 +1,30 @@
+'''
+1. Which exception is thrown when a string contains null?
+Answer:- NullPointerException
+'''
+
+'''
+2. The circular wait condition can be prevented by ___ 
+Answer:- defining a linear ordering of resource types 
+'''
+
+'''
+3. What will be the output of the following statement? 
+'''
+
+z = 5 > 4 or 2 > 3 or 3 < 2
+print(z)
+
+'''
+Output:- True
+'''
+
+'''
+4. 2 men and 3 boys can do a piece of work in 10 days and 3 men and 2 boys can do it in 8 days. How many days are required for 2 men and 1 boy to finish that work?
+Answer:- 12.5 
+'''
+
+'''
+5. If deadlocks occur frequently, the detection algorithm must be invoked ____ 
+Answer:- frequently
+'''

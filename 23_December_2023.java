44f637c6cc622f336f9234ef89a22c3bcc0236e5
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 19:42:40 +0530

    Add files via upload

---
+++ 23_December_2023.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+1. Mark (1) if the question can be answered by A alone but cannot be answered by B alone
+Mark (2) if the question can be answered by B alone but cannot be answered by A alone
+Mark (3) if the question cannot be answered by A or B alone but can be answered by combining the two statements.
+Mark (4) if the question can be answered by A alone and B alone 
+Mark (5) if the question cannot be answered by A or B alone and cannot be answered by combining the two statements. 
+What is the value of x?
+A: x2 = 64
+B: x3 = 512
+Answer:- 2
+*******************************************************************************/
+
+/******************************************************************************
+2. What will be the output of the following code?
+*******************************************************************************/
+
+public class Solution {
+    public static void func(int a, int b) {
+        System.out.println(a + " " + b);
+    }
+
+    public static void main(String arg[]) {
+        int a = 5;
+        int b = 15;
+        func(a);
+    }
+}
+
+/******************************************************************************
+Output:- Compilation Error 
+*******************************************************************************/
+
+/******************************************************************************
+3. Which of the syntax is correct to create a package?
+Answer:- package package_name;
+*******************************************************************************/
+
+/******************************************************************************
+4. A commonly used way to access private data members in a class is ____
+Answer:- Create public member functions to access those data members
+*******************************************************************************/
+
+/******************************************************************************
+5. A deadlock eventually cripples system throughput and will cause the CPU utilization to ____
+Answer:- drop
+*******************************************************************************/

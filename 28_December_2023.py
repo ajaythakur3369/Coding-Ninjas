aa604af360b13de3ffbd2e7120e7a926bfbe9226
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 20:46:43 +0530

    Add files via upload

---
+++ 28_December_2023.py
@@ -0,0 +1,44 @@
+'''
+2. Which type of comments are used to comment multiple lines of code? 
+Answer:- Multi-line comment 
+'''
+
+'''
+3. What will be the output of the following code?
+'''
+
+name = 'saif'
+d = int(name)
+print(type(d))
+
+'''
+Output:- Error 
+'''
+
+'''
+4. What will be the output of the following code? 
+'''
+
+try:
+    a = 10
+    b = 0
+    print(d)  
+    c = a / b
+except NameError:
+    print('Name Error occurred')
+except ZeroDivisionError:
+    print('Zero Division Error occurred')
+
+'''
+Output:- Name Error occurred 
+'''
+
+'''
+5. Creektulo means tree plantation
+creekfuos means tree mutation 
+fuosseed means mutation sapling 
+Which word means "plantation"?
+Answer:- Tulo
+'''
+
+

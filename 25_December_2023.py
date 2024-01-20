a60629ac582b26ac4968d38bcc91b6a934cd31bb
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 20:08:07 +0530

    Add files via upload

---
+++ 25_December_2023.py
@@ -0,0 +1,39 @@
+'''
+1. What is the purpose of a constructor when considering the concept of classes? 
+Answer:- It is defined to initialize the data members of an object when it is created
+'''
+
+'''
+2. A system has 3 processes sharing 4 resources. If each process needs a maximum of 2 units then, deadlock ____ 
+Answer:- can never occur 
+'''
+
+'''
+3. In each of the following questions, a statement/group of statements is given followed by some conclusions. Without resolving anything yourself choose the conclusion that logically follows from the given statements. 
+Statement: Soldiers serve their country. 
+Answer:- Some men who are soldiers serve their country.
+'''
+
+'''
+4. Which of the following commands is used to compile Java code?
+Answer:- javac file_Name.java
+'''
+
+'''
+5. What will be the output of the following code?
+'''
+
+try:
+    a = 10
+    b = 0
+    c = a / b
+    print(c)
+except ZeroDivisionError:
+    print("Exception occurred")
+    
+'''
+Output:- Exception occurred
+'''
+
+
+

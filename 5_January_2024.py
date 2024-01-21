6849ff11845650c9bb2ef1feaccedcd92a7e8da9
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-21 11:01:27 +0530

    Add files via upload

---
+++ 5_January_2024.py
@@ -0,0 +1,27 @@
+'''
+2. A pupil's marks were wrongly entered as 83 instead of 63. Due to that, the average marks for the class increased by half (1/2). The number of pupils in the class is
+Answer:- 40
+'''
+
+'''
+3. 10 women can complete a work in 7 days and 10 children take 14 days to complete the work. How many days will 5 women and 10 children take to complete the work? 
+Answer:- 7
+'''
+
+'''
+4. What will be the output of the following code?
+'''
+
+a = 7
+b = 7.0
+c = a * b
+print(type(c))
+
+'''
+Output:- <class 'float'>
+'''
+
+'''
+5. Is method overloading possible by changing the return type?
+Answer:- No
+'''

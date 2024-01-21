a3507470ac778b156794513d1e88a7585ce31765
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-21 11:16:26 +0530

    Add files via upload

---
+++ 7_January_2024.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+1. Which function is used for the print statements? 
+Answer:- print()
+*******************************************************************************/
+
+/******************************************************************************
+2. A one – dimensional array of one – dimensional arrays is called? 
+Answer:- Two – dimensional array 
+*******************************************************************************/
+
+/******************************************************************************
+3. The processes that are residing in the main memory and are ready and waiting to execute are kept on a list called ____ 
+Answer:- Ready Queue
+*******************************************************************************/
+
+/******************************************************************************
+4. What is the output of the following code?
+*******************************************************************************/
+
+public class CodingNinjas {
+    public static void main(String[] args) {
+        String str = "Coding Ninjas";
+        int len = str.length();
+        System.out.println(str.charAt(len - 1));
+    }
+}
+
+/******************************************************************************
+Output:- Runtime error
+*******************************************************************************/
+
+/******************************************************************************
+5. Divide Rs. 500 among A, B, C, and D so that A and B together get thrice as much as C and D together and B may get 4 times what C gets and C may get 112 times as much as D. What is the share of B. 
+Answer:- 300
+*******************************************************************************/

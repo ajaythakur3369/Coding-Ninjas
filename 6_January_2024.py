9a75422ab97185a05d7d836e592c8c40259227ce
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-21 11:07:37 +0530

    Add files via upload

---
+++ 6_January_2024.py
@@ -0,0 +1,24 @@
+'''
+1. What will be the output of the following Python code?
+'''
+
+def fun(name, roll):
+    print(name, roll)
+
+# Driver's code
+fun(name="parikh", roll=101)
+
+'''
+Output:- parikh 101
+'''
+
+'''
+2. What is the default value of int data type? 
+Answer:- 0 
+'''
+
+'''
+3. Which of the following is private IP?
+Answer:- 10.25.170.52 
+'''
+

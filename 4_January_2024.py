054f657d59d885d7f58d38dd7e3115ad947d0354
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-21 10:47:41 +0530

    Add files via upload

---
+++ 4_January_2024.py
@@ -0,0 +1,26 @@
+'''
+2. 18 men or 36 boys can complete a work in 24 days if they work 6 hours per day. How many days would be required if 24 men and 24 boys worked for 9 hours per day in the same job? 
+Answer:- 18
+'''
+
+'''
+3. Which of the following Scanner class methods is used to read String from the user?
+Answer:- nextLine()
+'''
+
+'''
+4. What will be the output of the following Python code?
+'''
+
+names1 = ['Aman', 'Afzal', 'Charlie']
+names2 = [name.lower() for name in names1]
+print(names2[2][0])
+
+'''
+Output:- c
+'''
+
+'''
+5. A computer system has 6 tape drives, with ‘n’ processes competing for them. Each process may need 3 tape drives. The maximum value of ‘n’ for which the system is guaranteed to be deadlock–free is? 
+Answer:- 4
+'''

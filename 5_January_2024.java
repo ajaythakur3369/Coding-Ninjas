e3954aa46efb05eb82a249a9001c693d07d7c6e9
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-21 11:01:10 +0530

    Add files via upload

---
+++ 5_January_2024.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+1. What will be the output of the following code?
+*******************************************************************************/
+
+public class Solution {
+    public static void main(String args[]) {
+        do {
+            System.out.println("Coding Ninjas");
+        } while (true);
+        System.out.println("Ninjas");
+    }
+}
+
+/******************************************************************************
+Output:- Infinite time Coding Ninjas
+*******************************************************************************/

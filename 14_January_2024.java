397b2a415bfb0a6c02373a51a4f6673258b198c9
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 11:28:17 +0530

    Add files via upload

---
+++ 14_January_2024.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+1. What is the output of the following code?
+*******************************************************************************/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s1 = "Coding";
+        String s2 = "Coding";
+        String s3 = new String("Coding");
+
+        System.out.print(s1 == s2);
+        System.out.print("");
+        System.out.print(s1 == s3);
+    }
+}
+
+/******************************************************************************
+Output:- true false
+*******************************************************************************/
+
+/******************************************************************************
+2. Tea worth Rs. 126 per kg and Rs. 135 per kg are mixed with a third variety in the ratio 1: 1: 2. If the mixture is worth Rs. 153 per kg, the price of the third variety per kg will be: 
+Answer:- Rs. 175.50 
+*******************************************************************************/
+
+/******************************************************************************
+3. Rohit walks at a speed of 12 kmph and he reaches the railway station 10 min after the train has gone and by walking at 15 kmph, he reaches at railway station 10 min before the train has gone. Find the distance from his home to the railway station.
+Answer:- 20
+*******************************************************************************/
+
+/******************************************************************************
+4. A+B+C = 20, A, B, C all are integers. How many solutions does it have if A >= 2, B>= 2, C>=2? 
+Answer:- 16C2+1
+*******************************************************************************/

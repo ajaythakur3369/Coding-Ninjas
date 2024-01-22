633ad7d1439e523f28926a596c24fe41ca07da18
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 10:12:07 +0530

    Add files via upload

---
+++ 10_January_2024.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+1. What is the output of the following code?
+*******************************************************************************/
+   
+public class Solution {
+    public static void main(String args[]) {
+        int x = 7 -- 2;
+        System.out.println(x);
+    }
+}
+
+/******************************************************************************
+Output:- Compilation error
+*******************************************************************************/ 
+
+/******************************************************************************
+2. In which of the following blocks which write the code that might throw an exception? 
+Answer:- try
+*******************************************************************************/ 
+  
+/******************************************************************************
+3. Which character is used in Python to make a single-line comment? 
+Answer:- #
+*******************************************************************************/ 
+  
+/******************************************************************************
+4. Bells toll together at 9:00 A.M. They toll at intervals of 7, 8, 11, and 12 seconds respectively. After 9:00 A.M., how many times will they toll together again in the next 3 hours?
+Answer:- 5
+*******************************************************************************/ 
+  
+/******************************************************************************
+5. A minimum of ___ variable (s) is/are required to be shared between processes to solve the critical section problem. 
+Answer:- two
+*******************************************************************************/

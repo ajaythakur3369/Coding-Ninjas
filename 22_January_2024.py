8205b3d9cfa2cd3b6a74708e8749424171151e4a
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 17:45:36 +0530

    Add files via upload

---
+++ 22_January_2024.py
@@ -0,0 +1,16 @@
+'''
+4. An exception is? 
+Answer:- an object 
+'''
+
+'''
+5. What will be the output of the following code?
+'''
+
+x = 1
+d = float(x)
+print(type(d))
+
+'''
+Output:- < class 'float' >
+'''

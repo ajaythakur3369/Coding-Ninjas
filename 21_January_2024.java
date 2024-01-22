06d368fff05cae05846ae39fd36aabe956e1e4e8
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 14:13:37 +0530

    Add files via upload

---
+++ 21_January_2024.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+1. What will be the output of the following code?
+*******************************************************************************/
+
+public class Solution {
+    public static void main(String args[]) {
+        int i = 2;
+
+        switch (i) {
+            case 1:
+                System.out.print("Case 1" + "");
+                break;
+            case 2:
+                System.out.print("Case 2" + "");
+            case 3:
+                System.out.print("Case 3" + "");
+            case 4:
+                System.out.print("Case 4" + "");
+        }
+    }
+}
+
+/******************************************************************************
+Output:- Case 2 Case 3 Case 4
+*******************************************************************************/
+
+/******************************************************************************
+2. Which of the following best describes the friend function in C++? 
+Answer:- A function that can access all the private, protected, and public members of a Class
+*******************************************************************************/
+
+/******************************************************************************
+3. Which of the following are arithmetic operators in Java?
+Answer:- All of the above (Addition and subtraction operator, Multiplication and division operator, Modulus operator)
+*******************************************************************************/
+
+/******************************************************************************
+4. Which of the following is true about overflow? 
+Answer:- Overflow in Java happens when we assign a value to a variable that is more than its range 
+*******************************************************************************/
+
+/******************************************************************************
+5. In the bakery algorithm to solve the critical section problem ____
+Answer:- each process receives a number (may or may not be unique) and the one with the lowest number is served next 
+*******************************************************************************/

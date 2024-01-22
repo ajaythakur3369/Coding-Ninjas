022938ceba4636428839869fdc76128546bd1382
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 13:37:48 +0530

    Add files via upload

---
+++ 18_January_2024.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+1. What will be the output of the following code?
+*******************************************************************************/
+
+public class Solution {
+    public static void main(String args[]) {
+        int i = 5;
+
+        switch (i) {
+            case 1:
+                System.out.print("Case 1" + "");
+                break;
+            case 2:
+                System.out.print("Case 2" + "");
+                break;
+            case 3:
+                System.out.print("Case 3" + "");
+                break;
+            default:
+                System.out.print("Value of i is greater than 3" + "");
+        }
+    }
+}
+
+/******************************************************************************
+Output:- Value of i is greater than 3 
+*******************************************************************************/
+

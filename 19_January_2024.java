c22a03775e6cdb6d8ec4c1834831b479c7034325
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 13:48:20 +0530

    Add files via upload

---
+++ 19_January_2024.java
@@ -0,0 +1,54 @@
+/******************************************************************************
+1. Which of the header files is used to implement algorithms provided by C++ STL?
+Answer:- <algorithm>
+*******************************************************************************/
+
+/******************************************************************************
+2. What is the output of the following code?
+*******************************************************************************/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String obj = "I" + "Java";
+        System.out.println(obj);
+    }
+}
+
+/******************************************************************************
+Output:- IlikeJava
+*******************************************************************************/
+
+/******************************************************************************
+3. What is the output of the following code?
+*******************************************************************************/
+
+public class Solution {
+    public Solution() {
+        System.out.println("Default constructor called");
+    }
+
+    public Solution(int x) {
+        System.out.println("Parameterized constructor called");
+    }
+
+    public static void main(String args[]) {
+        Solution obj = new Solution();
+    }
+}
+
+/******************************************************************************
+Output:- Default constructor called 
+*******************************************************************************/
+
+/******************************************************************************
+4. Statements: Wind is an inexhaustible source of energy and an aerogenerator can convert it into electricity. Though not much has been done in this field, the survey shows that there is vast potential for developing wind as an alternative source of energy 
+Conclusion:
+I. Energy by wind is a comparatively newly emerging field 
+II. The energy crisis can be dealt with by exploring more in the field of aerogeneration. 
+Answer:- Both I and II follow
+*******************************************************************************/
+
+/******************************************************************************
+5. A one – dimensional array of one – dimensional arrays is called? 
+Answer:- Two – dimensional array
+*******************************************************************************/

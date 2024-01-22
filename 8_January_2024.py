8d6c2b351493128b9adbbfbf3341a8d89db8965b
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 09:34:07 +0530

    Add files via upload

---
+++ 8_January_2024.py
@@ -0,0 +1,28 @@
+'''
+2. Which of the following conditions must be satisfied to solve the critical section problem? 
+Answer:- All of the above (Mutual Exclusion, Progress, Bounded Waiting)
+'''
+
+'''
+3. Which block lets you test a block of code for errors?
+Answer:- try
+'''
+
+'''
+4. What will be the output of the following code?
+'''
+
+roll_no = 10
+d = str(roll_no)
+print(type(d))
+
+'''
+Output:- <class ‘str’>
+'''
+
+'''
+5. Statement: “If you trouble me, I will slap you.” – A mother warns her child. 
+Assumption 1: With the warning, the child may stop troubling her.
+Assumption 2: All children are basically naughty. 
+Answer:- Only assumption I is implicit 
+'''

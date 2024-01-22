2bc7f22841397e236779150e395acb21148fd239
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 11:12:50 +0530

    Add files via upload

---
+++ 12_January_2024.py
@@ -0,0 +1,54 @@
+'''
+1. What will be the output of the following code?
+'''
+
+for i in range(1, 5):
+    if i == 3:
+        break
+    else:
+        print(i, end="")
+
+'''
+Output:- 1 2
+'''
+
+'''
+2. Which one of the following is the correct extension of the Python file?
+Answer:- .py
+'''
+
+'''
+3. What will be the output of the following code?
+'''
+
+i = 0
+while i < 5:
+    print(i, end="")
+
+'''
+Output:- Runtime Error 
+'''
+
+'''
+4. Study the following arrangement carefully and answer the question that follows: S K 6 & Q 2 R * C F 8 E $ G 2 # 4 9 L N 3 U V 5 Y ! B 7 W 9 Which of the following letters, numbers, or symbols will be third to the left or a fifth of the left? 
+Answer:- 6
+'''
+
+'''
+5. What will be the output of the following Python code? 
+'''
+
+a = 5
+b = 4
+print(a, end="")
+print(b)
+
+'''
+Output:- 5 4(single line)
+'''
+
+
+
+
+
+

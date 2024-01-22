a6774c6e5191c885b862670b776f078d7d09d666
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 11:21:27 +0530

    Add files via upload

---
+++ 13_January_2024.py
@@ -0,0 +1,28 @@
+'''
+2. What does 3 ^ 4 evaluate to?
+Answer:- 7
+'''
+
+'''
+3. John is stronger than Mike, but Luke is stronger than John. Statement: Mike is stronger than Luke. 
+Answer:- False
+'''
+
+'''
+4. The movie offended many of the parents of its younger viewers by including unnecessary ____ in the dialogue. 
+Answer:- vulgarity
+'''
+
+'''
+5. What will be the output of the following code?
+'''
+
+for i in range(1, 5):
+    if i == 5:
+        break
+    else:
+        print(i, end="")
+
+'''
+Output:- 1 2 3 4 
+'''

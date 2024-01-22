3a828d998526684380aa8b71d35000c8bc4aa3d6
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 13:38:08 +0530

    Add files via upload

---
+++ 18_January_2024.py
@@ -0,0 +1,24 @@
+'''
+2. Is it possible to overload a constructor by changing its return type?
+Answer: No
+'''
+
+'''
+3. Is it possible for one block of except statements to handle multiple exceptions?
+Answer:- yes, They can be defined as: except TypeError, SyntexError […]
+'''
+
+'''
+4. Price of a commodity has gone up by 40% and Shubham wants to limit his expenditure increase to 5%. What is the reduction in consumption, so that expenditure increases 5%? 
+Answer:- 25%
+'''
+
+'''
+5. What will be the output of the following code?
+'''
+
+chr(ord('A')
+
+'''
+Output:- A
+'''

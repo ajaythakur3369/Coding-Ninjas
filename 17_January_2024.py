6e7909e54246025b48619cd48c1e871dd4b8c635
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-22 13:25:44 +0530

    Add files via upload

---
+++ 17_January_2024.py
@@ -0,0 +1,26 @@
+'''
+2. Which of the following Type Casting is available in Java? 
+Answer:- Both a and b (Widening Type Casting, Narrowing Type Casting)
+'''
+
+'''
+3. Which of the following statement is true? 
+Answer:- All of the mentioned (Views could be looked as an additional layer on the table which enables us to protect intricate or sensitive data based upon our needs, Views are virtual tables that are compiled at run time, Creating views can improve query response time)
+'''
+
+'''
+4. What will be the output of the following statement?
+'''
+
+my_list = [1, 2, 3, 4, 5]
+ans = 3 not in my_list
+print(ans)
+
+'''
+Output:- False
+'''
+
+'''
+5. A train without its compartments runs at a speed of 42 kmph. The reduction in speed of the train is directly proportional to the square root of the number of compartments attached. When nine compartments were connected, the speed of the train was 24 kmph. What are the maximum numbers of compartments with which the train can just move? 
+Answer:- 48
+'''

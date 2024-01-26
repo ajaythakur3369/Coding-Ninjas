45098c6711d6e3ee49dc235c0e9a9c0897ff1512
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-26 11:02:29 +0530

    Add files via upload

---
+++ 26_January_2024.py
@@ -0,0 +1,36 @@
+'''
+2. Who developed the Python language?
+Answer:- Guido van Rossum
+'''
+
+'''
+3. What is the return type of a method that doesn't return any value?
+Answer:- void 
+'''
+
+'''
+4. What will be the output of the following statement?
+'''
+
+a = 2
+a *= 2
+b = a // 2
+print(a, b)
+
+'''
+Output:- 4 2
+'''
+
+'''
+5. What will be the output of the following statement?
+'''
+
+z = True and True
+print(z)
+
+'''
+Output:- True 
+'''
+
+
+

c667b27c93b96e514c90e8a4c217e7c8e850d164
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-29 08:40:19 +0530

    Add files via upload

---
+++ 29_January_2024.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+1. Which of the following are TCL commands?
+Answer:- ROLLBACK and SAVEPOINT
+*******************************************************************************/
+
+/******************************************************************************
+2. What will be the output of the following code?
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    for (i = 0; i < 10; i++); {
+        cout << i;
+    }
+    return 0;
+}
+
+/******************************************************************************
+Output:- 10
+*******************************************************************************/
+
+/******************************************************************************
+3. Which of the following statements about constructors is/are correct?
+Answer:- All the above (The constructor name should be the same as the class name., The default constructor invokes super() and sets all instance variables to a default value such as 0, null., 
+If we do not define a constructor for a class, the compiler will generate one for us, If we want to call parent class constructor, we must call it in the constructor's first line.)
+*******************************************************************************/
+
+/******************************************************************************
+4. Which among the following best defines single-level inheritance?
+Answer:- A class inheriting a base class
+*******************************************************************************/
---
+++ 29_January_2024.py
@@ -0,0 +1,9 @@
+'''
+5. What is the output of this following code? 
+'''
+
+"Hello".replace("l", "e")
+
+'''
+Output:- Heeeo
+'''

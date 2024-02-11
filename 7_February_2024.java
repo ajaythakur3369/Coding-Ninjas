daba8f9ce70617806a08a398203511832db27a09
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-11 20:07:29 +0530

    Add files via upload

---
+++ 7_February_2024.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+3. State true or false: Wait die scheme is a non-preemptive technique
+Answer:- True
+*******************************************************************************/
+
+/******************************************************************************
+4. A while loop in Java executes the statements at least once even if the condition is not satisfied. 
+Answer:- False
+*******************************************************************************/
+
+/******************************************************************************
+5. Which of the following commands is used to compile a package?
+Answer:- javac -d . class_name 
+*******************************************************************************/

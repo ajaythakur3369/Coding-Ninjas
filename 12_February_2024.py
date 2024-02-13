c62f063c25813f6a0bb65c73f8c9f821f561406b
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-13 16:42:20 +0530

    Add files via upload

---
+++ 12_February_2024.py
@@ -0,0 +1,40 @@
+'''
+1. What will be the output of the following Python code?
+'''
+
+def fun():
+    x=15
+    print(x)
+x=12
+fun()
+
+'''
+Output:- 15
+'''
+
+'''
+2. In the following question, four words are given out of which three are alike in some manner and one is different. Choose the odd one out. - 
+Faraday
+Newton
+Shashi Tharoor
+Edition
+Answer:- Shashi Tharoor
+'''
+
+'''
+3. A many-to-many relationship between two entities usually results in how many tables?
+Answer:- THREE
+'''
+
+'''
+4. Statements: The best evidence of India's glorious past is the growing popularity of Ayurvedic medicines in the West.
+Conclusions:
+1. Ayurvedic medicines are not popular in India
+2. Allopathic medicines are more popular in India.
+Answer:- Neither I nor II follows
+'''
+
+'''
+5. Which of the following data types is used to store the 1 + 3j?
+Answer:- complex
+'''

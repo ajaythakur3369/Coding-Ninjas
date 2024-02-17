98453e7814d92ebedb14bbe9c21d70c0bc3180a8
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-17 11:02:25 +0530

    Add files via upload

---
+++ 17_February_2024.py
@@ -0,0 +1,32 @@
+'''
+2. What will be the output of the following code?
+'''
+
+n = 5
+while(n > 0):
+    print(n,end = " ")
+    n = n - 1
+    
+'''
+Output:- 5 4 3 2 1
+'''
+
+'''
+3. The default CLASS C netmask is
+Answer:- 255.255.255.0
+'''
+
+'''
+4. Let A and B be two finite sets such that n(A) = 20, n(B) = 28 and n(A ∪ B) = 36, find n(A ∩ B).
+Answer:- 12
+'''
+
+'''
+5. Statements: Wind is an inexhaustible source of energy and an aerogenerator can convert it into electricity. Though not much has been done in this field, the survey shows that there is vast potential for developing wind as alternative source of energy
+
+Conclusion:
+1. Energy by wind is comparatively newly emerging field
+2. The energy crisis can be dealt by exploring more in the field of aero-generation.
+Answer:- Both I and II follow
+'''
+

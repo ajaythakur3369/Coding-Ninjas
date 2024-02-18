afe4d4fb4024c27111c8a96fecd2569087a2d8c8
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-02-18 18:23:54 +0530

    Add files via upload

---
+++ 18_February_2024.py
@@ -0,0 +1,38 @@
+'''
+1. What will be the output of the following statement?
+'''
+
+a = "5"
+b = 5
+print(a is not b)
+
+'''
+Output:- True
+'''
+
+'''
+2. Which of the following datatypes is used to store objects of different datatypes?
+Answer:- Class
+'''
+
+'''
+3. What will be the output of the following code?
+'''
+
+s="abcd"
+s[0]='c'
+print(s)
+
+'''
+Output:- Error
+'''
+
+'''
+4. Which among the following can be used together in a single class?
+Answer:- All three together
+'''
+
+'''
+5. X is a real number such that log3(5)=log5(2+x), which of the following is true.
+Answer:- 3<x<23
+'''

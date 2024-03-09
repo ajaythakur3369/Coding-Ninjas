4b68e9c38bd3b1a8a1fe32fccd4042f922e25b3b
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-03-10 00:47:56 +0530

    Add files via upload

---
+++ 10_March_2024.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+1. What will be the output of the following code?
+*******************************************************************************/
+
+public class Solution {
+
+    public int getSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String args[]) {
+
+        Solution obj = new Solution();
+        System.out.println(obj.getSum());
+    }
+}
+
+/******************************************************************************
+Output:- Compilation error
+*******************************************************************************/
---
+++ 10_March_2024.py
@@ -0,0 +1,25 @@
+'''
+2. What will be the output of the following code?
+'''
+
+roll_no = 10
+d = str(roll_no)
+print(type(d))
+
+'''
+Output:- < class 'str' >
+'''
+
+'''
+3. Creektulo means tree plantation
+creekfuos means tree mutation
+fuosseed means mutation sapling
+Which word means “plantation”?
+Answer:- Tulo
+'''
+
+'''
+4. At what time between 4 and 5, will the hands of a clock coincide? (Answer in minutes up to 2 decimal places):
+Answer:- 21.81
+'''
+
